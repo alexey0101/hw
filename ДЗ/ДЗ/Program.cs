b2b56f1c10ea4073087aef9ef1e1a4afaf7ce642
Alexey <timbersaw123987@gmail.com>
2020-02-08 02:39:53 +0300

    dz2222

--- ДЗ/ДЗ/Program.cs
+++ ДЗ/ДЗ/Program.cs
@@ -71,7 +71,6 @@ namespace ДЗ
 
                     calc.ErrorNotification += ConsoleErrorHandler;
                     calc.ErrorNotification += ResultErrorHandler;
-
                     calc.CalculateExpressions(); //Вычисление выражений из файла expressions.txt
                     ValidationCheck(); //Проверка значений
                     Console.WriteLine("Значения выражений подсчитаны и проверены.");
